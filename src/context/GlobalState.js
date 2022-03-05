0e45574e3f17cfd552e4b0bfbba6fbbc4c83aa12
Aaron Williams <polarizedcuriosity@gmail.com>
2022-03-04 17:16:05 -0800

    expense tracker app first commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,30 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer'
+
+
+
+//Initial state 
+const initialState = {
+  transactions: [
+    { id: 1, text: "flowers", amount: -20 },
+    {id: 2, text: "paycheck", amount: 3000 },
+    {id: 3, text: "protein", amount: -76 },
+    {id: 4, text: "photo-shoot", amount: 150 }
+  ]
+}
+
+
+//Create Context
+export const GlobalContext = createContext(initialState);
+
+
+//provider component 
+export const GlobalProvider = ({ children }) => {
+  const [ state, dispatch ] = useReducer(AppReducer, initialState);
+
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions
+  }}>
+    {children}
+  </GlobalContext.Provider>)
+}
