1950edd7256fa4f4a5bdb00f262560aa63a17dea
Aaron Williams <polarizedcuriosity@gmail.com>
2022-03-11 01:03:18 -0800

    added transaction component

---
+++ src/components/Transaction.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function Transaction()  {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+
+
+
+
+export default Transaction
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -8,9 +8,11 @@ function TransactionList()  {
     <>
       <h3>History</h3>
       <ul className="list">
+      {transactions.map(transactions => (
         <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li>
+          {transactions.text} <span>-$400</span><button className="delete-btn">x</button>
+        </li>))}
+
       </ul>
     </>
   )
