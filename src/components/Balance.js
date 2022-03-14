eec67752995f57d60309900ecb957397a8bde8fb
Aaron Williams <polarizedcuriosity@gmail.com>
2022-03-14 01:12:12 -0700

    use global context in balance.js to show total balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,14 +1,18 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { useContext } from "react/cjs/react.production.min";
+import { GlobalContext } from '../context./GlobalContext';
 
 
 function Balance()  {
+const { transactions } = useContext(GlobalContext);
 
+const amount = transactions.map(transaction => transaction.amount);
+const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   return(
     <div>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </div>
   )
 }
